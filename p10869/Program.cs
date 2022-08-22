fd9a14bf6c2fd881f02ac1868b41ceaa1323b7a0
kmijkoishi <ploy0620374@gmail.com>
2022-08-23 05:30:54 +0900

    Initial Commit.

---
+++ p10869/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace p10869
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string[] ss = s.Split();
+            int a = int.Parse(ss[0]);
+            int b = int.Parse(ss[1]);
+            Console.WriteLine(a+b);
+            Console.WriteLine(a-b);
+            Console.WriteLine(a*b);
+            Console.WriteLine(a/b);
+            Console.WriteLine(a%b);
+        }
+    }
+}
---
+++ p10926/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace p10926
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
